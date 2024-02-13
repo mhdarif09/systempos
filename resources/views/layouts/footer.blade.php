42d7e5d1ac14f4b2dd4bf9e94893bd5b2544defe
mhdarif09 <arifgaming2124@gmail.com>
2024-02-13 09:59:10 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>Triangle Pos © {{ date('Y') }} || Developed by <strong><a target="_blank" href="https://muhammadarifrahhmads.netlify.com">Muhammad Arif Rahmad S</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
